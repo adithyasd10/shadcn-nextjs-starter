3ee3c54a7a0f1ca4172859bc916d5b7c62f455db
Adithya S D <adithyasd@gmail.com>
2025-07-04 22:13:46 +0530

    trying shadcn hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center">
-      <Button>Click me</Button>
+     <HeroSection/>
     </div>
   );
 }
